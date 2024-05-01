8c5be87fd7eda02437fa4ebbfa816a99bda2ffce
Valeria Mironova <rimvelly@gmail.com>
2024-05-01 14:50:03 +0300

    шаг 8

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,8 +1,23 @@
 import readlineSync from 'readline-sync';
 
 const generateProgression = () => {
-    
-}
+    const firstNum = Math.floor(Math.random()*(30 - 1) + 1);
+    const progression = [firstNum];
+    const arrLength = Math.floor(Math.random()*(10 - 5) + 5);
+    const addNum = Math.floor(Math.random()*(5 - 2) + 2);
+
+    for (let i = 1; i < arrLength; i += 1) {
+        progression[i] = progression[i - 1] + addNum;
+    }
+    const elementIndex = Math.floor(Math.random() * arrLength);
+    let correctAnswer = '';
+    correctAnswer += `${progression[elementIndex]}`;
+    progression[elementIndex] = '...';
+    const question = progression.join(' ');
+
+
+    return [question, correctAnswer];
+  };
 
 
 const playBrainProgression = () => {
@@ -14,7 +29,7 @@ const playBrainProgression = () => {
     let correctAnswerCount = 0;
 
     for (let i = 0; i < 3; i += 1) {
-        const { question, correctAnswer } = generateProgression();
+        const [question, correctAnswer] = generateProgression();
         const userAnswer = readlineSync.question (`Question: ${question}\nYour answer: `);
 
         if (userAnswer === correctAnswer) {
