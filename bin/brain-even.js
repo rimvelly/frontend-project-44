a3a04dbe71ae8152d46359f0d47920d9ad0f94a6
Valeria Mironova <rimvelly@gmail.com>
2024-06-12 14:43:38 +0300

    правки

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import playBrainEven from "../src/games/brain-even.js";
+import playBrainEven from '../src/games/brain-even.js';
 
 playBrainEven();
-
-//+
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,72 +3,34 @@ import questionAndAnswer from '../index.js';
 const descriptions = 'What number is missing in the progression?';
 
 const generateProgression = (firstNum, arrLength, addNum) => {
-   
-    const progression = [firstNum];
-    for (let i = 1; i < arrLength; i += 1) {
-        progression[i] = progression[i - 1] + addNum;
-    }
-        return progression;
-  };
-
+  const progression = [firstNum];
+  for (let i = 1; i < arrLength; i += 1) {
+    progression[i] = progression[i - 1] + addNum;
+  }
+  return progression;
+};
 
 const playBrainProgression = () => {
-    const firstNum = Math.floor(Math.random()*(30 - 1) + 1);
-    const arrLength = Math.floor(Math.random()*(10 - 5) + 5);
-    const addNum = Math.floor(Math.random()*(5 - 2) + 2);
+  const firstNum = Math.floor(Math.random() * (30 - 1) + 1);
+  const arrLength = Math.floor(Math.random() * (10 - 5) + 5);
+  const addNum = Math.floor(Math.random() * (5 - 2) + 2);
 
-    const progression = generateProgression(firstNum, arrLength, addNum);
+  const progression = generateProgression(firstNum, arrLength, addNum);
 
-    const hideElement = Math.floor(Math.random()*(arrLength - 0) + 0);
+  const hideElement = Math.floor(Math.random() * (arrLength - 0) + 0);
 
-    let correctAnswer = '';
+  const correctAnswer = '';
 
-    correctAnswer += `${progression[hideElement]}`;
+  correctAnswer += `${progression[hideElement]}`;
 
-    progression[hideElement] = '...';
+  progression[hideElement] = '...';
 
-    let question = '';
+  let question = '';
 
-    for (let i = 0; i < arrLength; i += 1) {
-        question += `${progression[i]}`;
-    }
-    return [question, correctAnswer];
+  for (let i = 0; i < arrLength; i += 1) {
+    question += `${progression[i]}`;
+  }
+  return [question, correctAnswer];
 };
 
 export default () => { questionAndAnswer(descriptions, playBrainProgression); };
-
-
-
-
-/*
-import questionAndAnswer from '../index.js';
-
-const descriptions = 'What number is missing in the progression?';
-
-const generateProgression = () => {
-    const firstNum = Math.floor(Math.random()*(30 - 1) + 1);
-    const progression = [firstNum];
-    const arrLength = Math.floor(Math.random()*(10 - 5) + 5);
-    const addNum = Math.floor(Math.random()*(5 - 2) + 2);
-
-    for (let i = 1; i < arrLength; i += 1) {
-        progression[i] = progression[i - 1] + addNum;
-    }
-    
-    progression[elementIndex] = '...';
-    const question = progression.join(' ');
-
-
-   
-  };
-
-
-const playBrainProgression = () => {
-    const elementIndex = Math.floor(Math.random() * arrLength);
-    let correctAnswer = '';
-    correctAnswer += `${progression[elementIndex]}`;
-    return [question, correctAnswer];
-};
-
-export default () => { questionAndAnswer(descriptions, playBrainProgression); };
-*/
