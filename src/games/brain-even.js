96163ae8c381473f1ed80fe2443fe20c96357111
Valeria Mironova <rimvelly@gmail.com>
2024-09-03 23:15:00 +0300

    правки

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,8 +4,8 @@ const descriptions = 'Answer "yes" if the number is even, otherwise answer "no".
 
 const generateQuestion = () => {
   const randomNumber = Math.floor(Math.random() * 100);
-  const isEven = (randomNumber) => {
-    randomNumber % 2 === 0;
+  const isEven = (number) => {
+    return number % 2;
   };
   const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
   return [randomNumber, correctAnswer];
